c277312225d06ee885a812613c6b02b7c88aa207
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-17 16:42:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
   if (string === shout(string)) {
-    return
+    return "YES INDEED!"
   }
   if (string === ) {
     return
